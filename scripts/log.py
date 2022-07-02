03b683522d8eae6fd36ae22982d2e512e6840e55
rafaesam <rafaesam0@gmail.com>
2022-07-02 14:33:50 +0300

    log file

--- scripts/log.py
+++ scripts/log.py
@@ -3,8 +3,10 @@
 import sys, os
 import logging, logging.handlers
 from logging.handlers import TimedRotatingFileHandler
+from pathlib import Path
 formatter = logging.Formatter("%(asctime)s — %(name)s — %(levelname)s — %(message)s")
 log_file = "info.log"
+from config import Config
 
 # Logging Levels #
 ##############
@@ -15,23 +17,25 @@ log_file = "info.log"
 #  DEBUG     #
 ##############
 
-# Define Console Handler 
+
 def get_console_handler():
   console_handler = logging.StreamHandler(sys.stdout)
   console_handler.setFormatter(formatter)
   return console_handler
 
-#Create Handlers(Filehandler with filename)
+
 def get_file_handler():
-  file_handler = TimedRotatingFileHandler(log_file, when='midnight', utc=True)
+  file_handler = TimedRotatingFileHandler(Config.LOG_FILE, when='midnight')
   file_handler.setFormatter(formatter)
   return file_handler
 
-#Define Logger
+
 def get_logger(logger_name):
   logger = logging.getLogger(logger_name)
+  # better to have too much log than not enough
   logger.setLevel(logging.DEBUG)
   logger.addHandler(get_console_handler())
   logger.addHandler(get_file_handler())
+  # with this pattern, it's rarely necessary to propagate the error up to parent
   logger.propagate = False
   return logger
